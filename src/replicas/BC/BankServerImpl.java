c11b6f94bb245c45ec44f61453520532b3654c28
Jesse Tsang <Jessetsang@outlook.com>
2017-12-05 11:22:24 -0500

    Added concurrency functions
    for deposits, withdraw, UDP requests, etc

--- src/replicas/BC/BankServerImpl.java
+++ src/replicas/BC/BankServerImpl.java
@@ -1,6 +1,11 @@
 package replicas.BC;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -36,9 +41,16 @@ public class BankServerImpl extends FailureFreeFEPOA implements Runnable
 	
 	//UDP Server for listening incoming requests
 	private UDPServer UDPServer;
+	private DatagramSocket UDPServerSocket;
 	
 	//Holds other servers' addresses : ["ServerName", "hostName:portNumber"]
 	HashMap<String, String> serversList = new HashMap();
+	
+	//High Availability Flag
+	private boolean crashReplica = false;
+	
+	//Byzantine Failure Flag
+	private boolean errorReplica = false;
 	
 	private static final int CLIENT_NAME_INI_POS = 3;
 	
@@ -49,12 +61,13 @@ public class BankServerImpl extends FailureFreeFEPOA implements Runnable
 		this.UDPHost = branchID;
 		this.UDPPort = branchPort;
 		this.RMID = RMID;
-		
-		this.UDPServer = new UDPServer(branchID, branchPort, this);
-		
+			
 		//1.1 Logging Initiation
 		this.logger = this.initiateLogger();
 		this.logger.info("Server Log: | BankServerImpl Server Instance Creation | Branch: " + branchID + " | Port : " + UDPPort);
+		
+		//1.2 Start UDP Server
+		this.initiateUDP();
 		
 		//1.2 Method to generate test accounts here.
 		this.initiateAccount();
@@ -63,6 +76,8 @@ public class BankServerImpl extends FailureFreeFEPOA implements Runnable
 		System.out.println("Server Log: | BankServerImpl Server Instance Creation | Branch: " + branchID + " | Port : " + UDPPort);		
 	}
 	
+	
+
 	private Logger initiateLogger()
 	{
 		Logger logger = Logger.getLogger("Server Logs/" + this.branchID + "- Server Log");
@@ -97,6 +112,24 @@ public class BankServerImpl extends FailureFreeFEPOA implements Runnable
 		
 		return logger;
 	}
+	
+	private void initiateUDP()
+	{
+		try
+		{
+			UDPServerSocket = new DatagramSocket(UDPPort);
+			
+			this.UDPServer = new UDPServer(branchID, UDPPort, this);
+			
+			Thread UDPServerThread = new Thread(this.UDPServer);
+			UDPServerThread.start();
+		}
+		catch (SocketException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
 
 	
 	private void initiateAccount()
@@ -125,48 +158,53 @@ public class BankServerImpl extends FailureFreeFEPOA implements Runnable
 			String key = Character.toString((char)customerID.charAt(CLIENT_NAME_INI_POS));
 			ArrayList<Client> values = new ArrayList<Client>(10);
 			
-			//If a key doesn't exist ... for example no client with last name Z ... then ...
-			if(!clientList.containsKey(key))
-			{
-				//Adds a key and 10 buckets
-				clientList.put(key, values);
-			}
 			
-			//This is to test if the account is already exist.
-			//1. Extract the buckets of the last Name ... for example, we will get all the clients with last name start with Z
-			values = clientList.get(key);
-			
-			//2. After extracted the buckets, we loops to check if the customerID is already exist.
-			for (Client client: values)
+			synchronized (clientList)
 			{
-				if (client.getCustomerID().equals(customerID))
+				//If a key doesn't exist ... for example no client with last name Z ... then ...
+				if(!clientList.containsKey(key))
 				{
-					this.logger.severe("Server Log: | Account Creation Error: Account Already Exists | Customer ID: " + customerID);
+					//Adds a key and 10 buckets
+					clientList.put(key, values);
+				}
+				
+				//This is to test if the account is already exist.
+				//1. Extract the buckets of the last Name ... for example, we will get all the clients with last name start with Z
+				values = clientList.get(key);
+				
+				//2. After extracted the buckets, we loops to check if the customerID is already exist.
+				for (Client client: values)
+				{
+					if (client.getCustomerID().equals(customerID))
+					{
+						this.logger.severe("Server Log: | Account Creation Error: Account Already Exists | Customer ID: " + customerID);
+						
+						return false;
+					}
+				}
+				
+				//3. If no existing account found, then we create a new account.
+				Client newClient;
+				
+				try
+				{
+					newClient = new Client(firstName, lastName, address, phone, customerID, branchID);
+					values.add(newClient);
+					clientList.put(key, values);
+					
+					this.logger.info("Server Log: | Account Creation Successful | Customer ID: " + customerID);
+					this.logger.info(newClient.toString());
+					
+					return true;
+				}
+				catch (Exception e)
+				{
+					this.logger.severe("Server Log: | Account Creation Error. | Customer ID: " + customerID + " | " + e.getMessage());
 					
 					return false;
-				}
+				}		
 			}
-			
-			//3. If no existing account found, then we create a new account.
-			Client newClient;
-			
-			try
-			{
-				newClient = new Client(firstName, lastName, address, phone, customerID, branchID);
-				values.add(newClient);
-				clientList.put(key, values);
 				
-				this.logger.info("Server Log: | Account Creation Successful | Customer ID: " + customerID);
-				this.logger.info(newClient.toString());
-				
-				return true;
-			}
-			catch (Exception e)
-			{
-				this.logger.severe("Server Log: | Account Creation Error. | Customer ID: " + customerID + " | " + e.getMessage());
-				
-				return false;
-			}	
 		}//end if clause ... if not the same branch
 		else
 		{
@@ -194,15 +232,18 @@ public class BankServerImpl extends FailureFreeFEPOA implements Runnable
 			
 			for (Client client : values)
 			{
-				if (client.getCustomerID().equals(customerID))
+				synchronized(client)
 				{
-					client.setBalance(amount);
-					double newBalance = client.getBalance();
-					
-					this.logger.info("Server Log: | Deposit Log: | Deposit: " + amount + " | Balance: " + newBalance + " | Customer ID: " + customerID);
-					
-					return true;
-				}
+					if (client.getCustomerID().equals(customerID))
+					{
+						client.setBalance(amount);
+						double newBalance = client.getBalance();
+						
+						this.logger.info("Server Log: | Deposit Log: | Deposit: " + amount + " | Balance: " + newBalance + " | Customer ID: " + customerID);
+						
+						return true;
+					}					
+				}				
 			}
 		}
 		catch (Exception e)
@@ -233,33 +274,38 @@ public class BankServerImpl extends FailureFreeFEPOA implements Runnable
 			String key = Character.toString((char)customerID.charAt(CLIENT_NAME_INI_POS));
 			ArrayList<Client> values = clientList.get(key);
 			
+			
+			
 			for (Client client : values)
 			{
-				if (client.getCustomerID().equals(customerID))
+				synchronized(client)
 				{
-					float oldBalance = client.getBalance();
-					float newBalance = oldBalance - amount;				
-					
-					if (newBalance < 0 )
+					if (client.getCustomerID().equals(customerID))
 					{
-						this.logger.severe("Server Log: | Withdrawl Error: Attempted to withdraw more than current balance. | Amount: " 
-								+ amount + " | Customer Balance: " + oldBalance + " | Customer ID: " + customerID);
+						float oldBalance = client.getBalance();
+						float newBalance = oldBalance - amount;				
 						
-						System.out.println("Server Log: | Withdrawl Error: Attempted to withdraw more than current balance. | Amount: " 
-								+ amount + " | Customer Balance: " + oldBalance + " | Customer ID: " + customerID);
-						
-						return false;
-					}
-					else
-					{
-						client.setBalance(newBalance);
-						
-						this.logger.info("Server Log: | Withdrawl Log: | Withdrawl: " + amount + " | Balance: " + newBalance + " | Customer ID: " + customerID);
-						System.out.println("Server Log: | Withdrawl Log: | Withdrawl: " + amount + " | Balance: " + newBalance + " | Customer ID: " + customerID);
-						
-						return true;
-					}					
-				}
+						if (newBalance < 0 )
+						{
+							this.logger.severe("Server Log: | Withdrawl Error: Attempted to withdraw more than current balance. | Amount: " 
+									+ amount + " | Customer Balance: " + oldBalance + " | Customer ID: " + customerID);
+							
+							System.out.println("Server Log: | Withdrawl Error: Attempted to withdraw more than current balance. | Amount: " 
+									+ amount + " | Customer Balance: " + oldBalance + " | Customer ID: " + customerID);
+							
+							return false;
+						}
+						else
+						{
+							client.setBalance(newBalance);
+							
+							this.logger.info("Server Log: | Withdrawl Log: | Withdrawl: " + amount + " | Balance: " + newBalance + " | Customer ID: " + customerID);
+							System.out.println("Server Log: | Withdrawl Log: | Withdrawl: " + amount + " | Balance: " + newBalance + " | Customer ID: " + customerID);
+							
+							return true;
+						}					
+					}		
+				}		
 			}
 		}
 		catch (Exception e)
@@ -286,14 +332,17 @@ public class BankServerImpl extends FailureFreeFEPOA implements Runnable
 					
 			for (Client client : values)
 			{
-				if (client.getCustomerID().equals(customerID))
+				synchronized (client)
 				{
-					newBalance = client.getBalance();
-							
-					this.logger.info("Server Log: | Balance Log: | Balance: " + newBalance + " | Customer ID: " + customerID);
-					
-					return newBalance;
-				}
+					if (client.getCustomerID().equals(customerID))
+					{
+						newBalance = client.getBalance();
+								
+						this.logger.info("Server Log: | Balance Log: | Balance: " + newBalance + " | Customer ID: " + customerID);
+						
+						return newBalance;
+					}			
+				}			
 			}
 		}
 		catch (Exception e)
@@ -445,10 +494,50 @@ public class BankServerImpl extends FailureFreeFEPOA implements Runnable
 		this.UDPServer.stop();	
 	}
 	
+	//Each replica will have a thread dedicated for receiving requests from other replica
+	//Thread is created at the time of replica creation, each thread represents a concurrent replica
+	//The UDPServer class will receive the request, get the BankServerImpl object, invoke the method
+	//The thread will registers its port number to the UDP registry for lookup 
 	@Override
 	public void run()
 	{
-		// TODO Auto-generated method stub		
+		//1. Register port to UDP registry
+		byte[] registryBuffer = ("0" + ";;" + branchID).getBytes();
+		
+		try
+		{
+			DatagramPacket requestPackage = new DatagramPacket(registryBuffer, registryBuffer.length, InetAddress.getLocalHost(), 8000);
+			
+			UDPServerSocket.send(requestPackage);		
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//Run as long as no High Availability flag
+		while(!crashReplica)
+		{
+			byte[] requestBuffer = new byte[1024];
+			DatagramPacket requests = new DatagramPacket(requestBuffer, requestBuffer.length);
+			
+			try
+			{
+				UDPServerSocket.receive(requests);
+				
+				//Create a new thread for each request
+				UDPServer replicaServerThread = new UDPServer(UDPHost, UDPPort, this);
+				
+				Thread requestThread = new Thread(replicaServerThread);
+				requestThread.start();
+			}
+			catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+		}	
 	}
 	
 	public int getClientCount()
@@ -482,4 +571,26 @@ public class BankServerImpl extends FailureFreeFEPOA implements Runnable
 	{
 		return RMID;
 	}
+
+	public boolean isCrashReplica()
+	{
+		return crashReplica;
+	}
+
+	public void setCrashReplica(boolean crashReplica)
+	{
+		this.crashReplica = crashReplica;
+	}
+
+	public boolean isErrorReplica()
+	{
+		return errorReplica;
+	}
+
+	public void setErrorReplica(boolean errorReplica)
+	{
+		this.errorReplica = errorReplica;
+	}
+	
+	
 }
